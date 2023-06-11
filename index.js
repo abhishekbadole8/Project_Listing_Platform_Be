9a222b8eb1e858a9abf3f32825d83811b4a2f2c1
Abhishek Badole <abhishekbadole8@gmail.com>
2023-06-11 20:30:26 +0530

    clear 5

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require('cors');
 const app = express();
 const dotenv = require("dotenv").config();
-const PORT = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 const bodyParser = require("body-parser");
 
 const connectDb = require("./src/config/dbConnection");
